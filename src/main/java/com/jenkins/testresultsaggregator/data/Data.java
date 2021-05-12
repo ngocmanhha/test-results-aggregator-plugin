bed1643385cefbfccfd9b9871ce893e3cd2d176b
Socrates <socrates.sidereas@gmail.com>
2021-05-12 11:54:04 +0300

    ADD : Options to ignore jobs with status not found,aborted and disabled

--- src/main/java/com/jenkins/testresultsaggregator/data/Data.java
+++ src/main/java/com/jenkins/testresultsaggregator/data/Data.java
@@ -1,6 +1,7 @@
 package com.jenkins.testresultsaggregator.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -64,6 +65,14 @@ public class Data extends AbstractDescribableImpl<Data> implements Serializable 
 	@DataBoundSetter
 	public void setReportGroup(ReportGroup reportGroup) {
 		this.reportGroup = reportGroup;
+	}
+	
+	public Data copy() {
+		Data dt = new Data();
+		dt.setGroupName(groupName);
+		dt.setReportGroup(reportGroup);
+		dt.setJobs(new ArrayList<>(jobs));
+		return dt;
 	}
 	
 }
