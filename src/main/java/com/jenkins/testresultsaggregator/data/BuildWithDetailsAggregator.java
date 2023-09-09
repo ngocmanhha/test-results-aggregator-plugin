e5fc5cb2c52f745a853658a3fd4fcb3a43c96385
sokratis.sidereas <sokratis.sidereas@6B9B043.internal.upstreamsystems.com>
2023-09-09 20:23:37 +0300

    FIX : find bugs issue

--- src/main/java/com/jenkins/testresultsaggregator/data/BuildWithDetailsAggregator.java
+++ src/main/java/com/jenkins/testresultsaggregator/data/BuildWithDetailsAggregator.java
@@ -36,4 +36,41 @@ public class BuildWithDetailsAggregator extends BuildWithDetails {
 		this.results = results;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((buildDetails == null) ? 0 : buildDetails.hashCode());
+		result = prime * result + ((buildNumber == null) ? 0 : buildNumber.hashCode());
+		result = prime * result + ((results == null) ? 0 : results.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BuildWithDetailsAggregator other = (BuildWithDetailsAggregator) obj;
+		if (buildDetails == null) {
+			if (other.buildDetails != null)
+				return false;
+		} else if (!buildDetails.equals(other.buildDetails))
+			return false;
+		if (buildNumber == null) {
+			if (other.buildNumber != null)
+				return false;
+		} else if (!buildNumber.equals(other.buildNumber))
+			return false;
+		if (results == null) {
+			if (other.results != null)
+				return false;
+		} else if (!results.equals(other.results))
+			return false;
+		return true;
+	}
+	
 }
