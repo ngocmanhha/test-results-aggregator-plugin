ca8d81b3d07cb0e4fce597635fe71411da9f2f22
sokratis.sidereas <sokratis.sidereas@host.docker.internal>
2023-12-06 15:30:55 +0200

    CH : comment out thread counter

--- src/main/java/com/jenkins/testresultsaggregator/actions/Collector.java
+++ src/main/java/com/jenkins/testresultsaggregator/actions/Collector.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import com.google.common.base.Stopwatch;
@@ -35,7 +36,7 @@ public class Collector {
 	public static final int parrallelThreads = 6;
 	public static final int delayThreads = 6000;
 	public static final int maxThreadTime = 120000;
-	
+	public AtomicInteger threadCounter = new AtomicInteger(0);
 	// Urls
 	public static final String DEPTH = "?depth=1";
 	
@@ -204,7 +205,7 @@ public class Collector {
 		ReportThread[] threads = new ReportThread[allDataJobDTO.size()];
 		int index = 0;
 		for (Job tempDataJobDTO : allDataJobDTO) {
-			threads[index] = new ReportThread(tempDataJobDTO, compareWithPreviousRun, ignoreRunningJobs);
+			threads[index] = new ReportThread(tempDataJobDTO, compareWithPreviousRun, ignoreRunningJobs, allDataJobDTO.size());
 			index++;
 		}
 		index = 0;
@@ -226,17 +227,20 @@ public class Collector {
 		Job job;
 		boolean compareWithPreviousRun;
 		boolean ignoreRunningJobs;
+		int total;
 		
-		public ReportThread(Job job, boolean compareWithPreviousRun, boolean ignoreRunningJobs) {
+		public ReportThread(Job job, boolean compareWithPreviousRun, boolean ignoreRunningJobs, int total) {
 			this.job = job;
 			this.compareWithPreviousRun = compareWithPreviousRun;
 			this.ignoreRunningJobs = ignoreRunningJobs;
+			this.total = total;
 		}
 		
 		@Override
 		public void run() {
 			Stopwatch stopwatch = Stopwatch.createStarted();
 			StringBuilder text = new StringBuilder();
+			// text.append(threadCounter.incrementAndGet() + "/" + total + " ");
 			if (job.getModelJob() != null) {
 				try {
 					job.setJob(getDetails(job));
